0dbf77c363f32ab393f1bdf06011db17d2a94596
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2024-12-17 13:04:46 +0100

    menu ouverture et fermeture avec animation

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const toggle = document.querySelector(".menu__btn");
+const closeBtn = document.querySelector(".menu__close--btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+        const isOpen = toggle.ariaExpanded === "true";
+        const isClosed = !isOpen;
+        toggle.ariaExpanded = isClosed;
+        nav.ariaHidden = isOpen;
+        page.classList.toggle("noscroll", isClosed);
+    });
+}
+
+closeBtn.addEventListener("click", () => {
+    nav.ariaHidden = true;
+    toggle.ariaExpanded = false;
+    body.classList.remove("noscroll");
+});
+
